efddbf938e32cbbb31983e33579d1d52e6478ffa
the-coderx <abhishekr.freelance224@gmail.com>
2026-01-12 22:57:29 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,9 +17,6 @@ resource "aws_s3_bucket" "app_bucket" {
   acl    = "public-read"                        # Issue 1: public-read ACL
 }
 
-# FIX APPLIED: Restricted IAM policy to follow principle of least privilege
-# Replaced wildcard "*" actions and resources with specific, scoped permissions
-# This addresses CWE-285 and prevents full administrative privileges
 resource "aws_iam_policy" "app_policy" {
   name        = "app-full-access"
   description = "Policy used by instances"
@@ -33,12 +30,11 @@ resource "aws_iam_policy" "app_policy" {
       "Action": [
         "s3:GetObject",
         "s3:PutObject",
-        "s3:ListBucket"
+        "s3:ListBucket",
+        "ec2:DescribeInstances",
+        "ec2:DescribeVolumes"
       ],
-      "Resource": [
-        "arn:aws:s3:::sample-app-terraform-bucket-12345",
-        "arn:aws:s3:::sample-app-terraform-bucket-12345/*"
-      ]
+      "Resource": "*"                            # Issue 3: wildcard resources
     }
   ]
 }
